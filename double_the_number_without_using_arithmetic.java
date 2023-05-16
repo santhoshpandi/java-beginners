cfe9b0a00649bbedea17101ffe1b7120eb92460f
Santhosh Pandi <109999710+santhoshpandi@users.noreply.github.com>
2023-05-16 07:25:34 +0530

    Add files via upload

---
+++ double_the_number_without_using_arithmetic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class double_the_number_without_using_arithmetic{
+	public static void main(String[] args) {
+		Scanner c = new Scanner(System.in);
+		int n=c.nextInt();
+		//by using bitwise left shift operator
+		System.out.println(n<<1);
+	}
+}
+
+/*
+
+write a program to double the given number without using arithmetic operators
+
+*/
